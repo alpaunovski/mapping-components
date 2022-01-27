0c3694875295b4078dcd31d6f245e3d93f079ddb
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-01-27 12:40:06 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map(contact => (
+        <Card
+          key={contact.id}
+          keyName={contact.id}
+          name={contact.name}
+          img={contact.imgURL}
+          tel={contact.phone}
+          email={contact.email}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
